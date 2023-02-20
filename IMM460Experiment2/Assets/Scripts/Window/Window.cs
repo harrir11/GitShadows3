f4f2c58ceec8f4ff5a119d4104134ae1205d18f3
leo23230 <54864620+leo23230@users.noreply.github.com>
2023-02-20 16:36:08 -0500

    Curtain Reveal Shadow Test

--- IMM460Experiment2/Assets/Scripts/Window/Window.cs
+++ IMM460Experiment2/Assets/Scripts/Window/Window.cs
@@ -7,10 +7,16 @@ public class Window : MonoBehaviour
 {
     public UnityEngine.Rendering.Universal.Light2D mainLight;
     public UnityEngine.Rendering.Universal.Light2D rayLight;
-    private float targetIntensity = .20f;
+    private float targetIntensity = .40f;
     private bool curtainIsOpen = false;
+    public Transform curtainTransform;
+    private float curtainX;
 
     // Update is called once per frame
+    private void Start()
+    {
+        curtainX = curtainTransform.position.x;
+    }
     void Update()
     {
         if(Input.GetKeyDown("space")) {
@@ -26,6 +32,13 @@ public class Window : MonoBehaviour
         float currentIntensity = rayLight.intensity;
         if(currentIntensity < targetIntensity){
             rayLight.intensity += 0.001f;
+
+            //this is what actually changes the curtain's x value
+            curtainX += .00625f;
+            //this vector 3 is creating a vector 3 with the adjusted curtain x, and 
+            //were just filling y and z with the current values since those arent changing
+            Vector3 newCurtainPosition = new Vector3(curtainX, curtainTransform.position.y, curtainTransform.position.y);
+            curtainTransform.position = newCurtainPosition;
         }
         else{
             curtainIsOpen = false;
