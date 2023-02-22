48f1594a3e02f04ae5902243b012c62f435a7caa
harrir11 <harrir11@tcnj.edu>
2023-02-22 14:48:49 -0500

    testing re-organization

---
+++ IMM460Experiment2/Assets/Scripts/Point_and_Click_CH/PlayerMovement.cs
@@ -0,0 +1,76 @@
+/*
+    Name: Connie Huang
+    Last modified: 2023-02-21
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Color playAreaColor = Color.green;
+
+    public Vector2[] playAreaPoints;
+
+    private Vector3 targetPosition;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = playAreaColor;
+
+        if (playAreaPoints != null && playAreaPoints.Length >= 2)
+        {
+            for (int i = 0; i < playAreaPoints.Length; i++)
+            {
+                int j = (i + 1) % playAreaPoints.Length;
+                Gizmos.DrawLine(playAreaPoints[i], playAreaPoints[j]);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Plane groundPlane = new Plane(Vector3.forward, Vector3.zero);
+            float distance;
+            if (groundPlane.Raycast(ray, out distance))
+            {
+                Vector3 hitPoint = ray.GetPoint(distance);
+                targetPosition = new Vector3(hitPoint.x, hitPoint.y, transform.position.z);
+            }
+        }
+        
+        // Check if the target position is inside the play area
+        if (IsPointInsidePlayArea(targetPosition))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+    }
+
+    private bool IsPointInsidePlayArea(Vector3 point)
+    {
+        int intersectCount = 0;
+
+        // Check how many times a line from the point intersects with the play area edges
+        for (int i = 0; i < playAreaPoints.Length; i++)
+        {
+            int j = (i + 1) % playAreaPoints.Length;
+
+            if (((playAreaPoints[i].y <= point.y) && (point.y < playAreaPoints[j].y))
+                || ((playAreaPoints[j].y <= point.y) && (point.y < playAreaPoints[i].y)))
+            {
+                float xIntersection = (point.y - playAreaPoints[i].y) / (playAreaPoints[j].y - playAreaPoints[i].y) * (playAreaPoints[j].x - playAreaPoints[i].x) + playAreaPoints[i].x;
+                if (point.x < xIntersection)
+                {
+                    intersectCount++;
+                }
+            }
+        }
+
+        return (intersectCount % 2 == 1);
+    }
+}
