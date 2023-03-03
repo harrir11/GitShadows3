3affbf10a05b7cca766b753cf43680d83145c6a0
harrir11 <harrir11@tcnj.edu>
2023-03-03 01:39:41 -0500

    sanity bar

---
+++ IMM460Experiment2/Assets/GDT-LinearIndicator/ExampleScriptLinearIndicator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScriptLinearIndicator : MonoBehaviour
+{
+    //This script is an example on how to use the linear indicator asset
+
+    public LinearIndicator linearIndicator;
+    public LinearIndicator linearIndicator2, linearIndicator3, linearIndicator4, linearIndicator5, linearIndicator6;
+
+    public float minValue, maxValue;
+    public float currentValue;
+
+    void Start()
+    {
+        //Setup the linear indicator by code or do it in inspector
+        linearIndicator.SetupIndicator(minValue,maxValue);
+        linearIndicator2.SetupIndicator(minValue, maxValue);
+        linearIndicator3.SetupIndicator(minValue, maxValue);
+        linearIndicator4.SetupIndicator(minValue, maxValue);
+        linearIndicator5.SetupIndicator(minValue, maxValue);
+        linearIndicator6.SetupIndicator(minValue, maxValue);
+
+
+        //linearIndicator.SetOrientation(LinearIndicator.Orientation.Horizontal);
+        //linearIndicator.reverse = false;
+        linearIndicator.SetValue(currentValue);
+        linearIndicator2.SetValue(currentValue);
+        linearIndicator3.SetValue(currentValue);
+        linearIndicator4.SetValue(currentValue);
+        linearIndicator5.SetValue(currentValue);
+        linearIndicator6.SetValue(currentValue);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentValue += 50*Time.deltaTime;
+            currentValue = Mathf.Clamp(currentValue, minValue, maxValue);
+
+
+            linearIndicator.SetValue(currentValue);//Use the function SetValue to give the indicator the value to display
+            linearIndicator2.SetValue(currentValue);
+            linearIndicator3.SetValue(currentValue);
+            linearIndicator4.SetValue(currentValue);
+            linearIndicator5.SetValue(currentValue);
+            linearIndicator6.SetValue(currentValue);
+
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentValue -= 50 * Time.deltaTime;
+            currentValue = Mathf.Clamp(currentValue, minValue, maxValue);
+
+
+            linearIndicator.SetValue(currentValue);
+            linearIndicator2.SetValue(currentValue);
+            linearIndicator3.SetValue(currentValue);
+            linearIndicator4.SetValue(currentValue);
+            linearIndicator5.SetValue(currentValue);
+            linearIndicator6.SetValue(currentValue);
+
+        }
+    }
+}
---
+++ IMM460Experiment2/Assets/GDT-LinearIndicator/LinearIndicator.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearIndicator : MonoBehaviour
+{
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical
+    }
+    [SerializeField]
+    public Orientation orientation;
+    [SerializeField]
+    public bool reverse;
+
+    public Color fillColor=Color.green;
+    public bool useTwoColors;
+    public Color emptyColor = Color.red;
+
+    private UnityEngine.UI.Image indicatorImage;
+    public float minValue, maxValue;
+    private float currentValue,currentScale;
+    [SerializeField]
+    private RectTransform valueIndicator;
+
+    void Awake()
+    {
+        indicatorImage = valueIndicator.GetComponent<UnityEngine.UI.Image>();
+        indicatorImage.color = fillColor;
+        if (reverse)
+        {
+            valueIndicator.pivot = Vector2.one;
+        }
+        else
+        {
+            valueIndicator.pivot = Vector2.zero;
+        }
+
+    
+    }
+
+    void Update()
+    {
+
+
+    }
+
+    public void SetupIndicator(float minValue, float maxValue)
+    {
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
+
+    public void SetOrientation(Orientation o)
+    {
+        orientation = o;
+    }
+
+
+    public void SetValue(float v)
+    {
+        currentValue = Mathf.Clamp(v,minValue,maxValue);
+        currentScale = (currentValue - minValue) / (maxValue - minValue);
+
+        if (useTwoColors)
+        {
+            indicatorImage.color = Color.Lerp(emptyColor,fillColor,currentScale);
+        }
+
+        if (reverse)
+        {
+            valueIndicator.pivot = Vector2.one;
+        }
+        else
+        {
+            valueIndicator.pivot = Vector2.zero;
+        }
+
+        switch (orientation)
+        {
+            case Orientation.Horizontal:
+                valueIndicator.localScale = Vector3.right * currentScale + Vector3.up+Vector3.forward;
+                break;
+            case Orientation.Vertical:
+                valueIndicator.localScale = Vector3.up * currentScale + Vector3.forward+Vector3.right;
+                break;
+
+        }
+
+    }
+
+}
