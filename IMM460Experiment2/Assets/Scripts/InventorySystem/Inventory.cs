e5f64146eeb956fc50049c771909aeefd5744e2a
Connie Huang <huangc8@tcnj.edu>
2023-04-01 19:32:08 -0400

    Added drop item feature

--- IMM460Experiment2/Assets/Scripts/InventorySystem/Inventory.cs
+++ IMM460Experiment2/Assets/Scripts/InventorySystem/Inventory.cs
@@ -12,14 +12,4 @@ public class Inventory : MonoBehaviour
 {
     public bool[] isFull;
     public GameObject[] slots;
-
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
 }
