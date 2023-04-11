a37016ec455b61f690b3a5b4b757c730f5e16050
Connie Huang <huangc8@tcnj.edu>
2023-04-11 16:03:01 -0400

    WIP: Select Item

---
+++ IMM460Experiment2/Assets/Scripts/InventorySystem/SelectItem.cs
@@ -0,0 +1,29 @@
+/* 
+    Name: Connie Huang
+    Last modified: 2023-04-11
+    Tutorial followed: https://youtu.be/DLAIYSMYy2g
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectItem : MonoBehaviour
+{
+    // private Transform itemPosition = transform.position;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void Use()
+    {
+
+    }
+}
