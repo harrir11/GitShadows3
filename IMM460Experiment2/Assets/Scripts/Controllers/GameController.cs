ee7e661c9da0d472522bb2788d2750c00ea84a45
harrir11 <harrir11@tcnj.edu>
2023-04-13 01:42:40 -0400

    minor experiment

--- IMM460Experiment2/Assets/Scripts/Controllers/GameController.cs
+++ IMM460Experiment2/Assets/Scripts/Controllers/GameController.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    private GameObject dinosaurButton;
+
+
+public bool dinoSelected = false;
+
 void Awake()
 {
     DontDestroyOnLoad(transform.gameObject);
+}
+
+public void objSelected(){
+    //if (Selection.activeGameObject != null) {
+        dinoSelected = true;
+        Debug.Log("dino selected");
+    //} else {
+        //dinoSelected = false;
+        //Debug.Log("dino not selected");
+    //}
+
+}
+
+public void targetSelected(){
+    if(dinoSelected) {
+        Debug.Log("MATCH!");
+        //dinosaurButton = GameObject.Find("DinosaurButton");
+        
+        //Destroy(dinosaurButton);
+    }
 }
 
 }
